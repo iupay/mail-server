557defd255793b905adb1580a1db67302436e2bf
Gabriel Baptistucci Garcia <gabrielbgarcia0999@gmail.com>
2021-11-30 17:11:46 -0300

    fix: command check

--- install.sh
+++ install.sh
@@ -18,20 +18,21 @@ set -e
 apt-get update
 apt-get upgrade -y
 
-if ! command -v node &> /dev/null; then
+
+if ! [ -x "$(command -v node)" ]; then
     echo "\"node\" not found. Installing..."
     curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
     apt-get install -y nodejs build-essential
 fi
 
-if ! command -v yarn &> /dev/null; then
+if ! [ -x "$(command -v yarn)" ]; then
     echo "\"yarn\" not found. Installing..."
     curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null
     echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list
     apt-get update && apt-get install -y yarn
 fi
 
-if ! command -v haraka &> /dev/null; then
+if ! [ -x "$(command -v haraka)" ]; then
     echo "\"haraka\" not found. Installing..."
     apt-get install -y python3
     yarn global add Haraka @google-cloud/pubsub
